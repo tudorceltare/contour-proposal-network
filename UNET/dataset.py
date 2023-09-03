e9e0d70e4c2dd3cac13d11c48fe531746a207444
tudor <tudor.pitic@gmail.com>
2023-09-03 14:14:07 +0300

    Implemented everything against God's will

--- UNET/dataset.py
+++ UNET/dataset.py
@@ -1,126 +1,17 @@
 import os
-from os.path import join
-from imageio import imread
-import cv2
-from collections import OrderedDict
 from PIL import Image
 from torch.utils.data import Dataset
 import numpy as np
-from skimage import measure
-from skimage.measure import regionprops
 
-
-# def read_all(directory, filename):
-#     if filename is None or directory is None:
-#         return None
-#     with open(join(directory, filename), 'r') as f:
-#         return [i.strip() for i in f.readlines()]
-#
-#
-# def load(images_directory, masks_directory, names, **label_kwargs):
-#     if None in (images_directory, masks_directory):
-#         return None, None, None
-#     images = [imread(join(images_directory, f.replace('.png', '.tif'))) for f in names]
-#     masks = [imread(join(masks_directory, f)) for f in names]
-#     return images, masks, [measure.label(m[:, :, 0], **label_kwargs) for m in masks]
-#
-# def labels2contours(labels, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE, flag_fragmented_inplace=False,
-#                     raise_fragmented=True, constant=-1) -> dict:
-#     """Labels to contours.
-#
-#     Notes:
-#         - If ``flag_fragmented_inplace is True``, ``labels`` may be modified inplace.
-#
-#     Args:
-#         labels:
-#         mode:
-#         method: Contour method. CHAIN_APPROX_NONE must be used if contours are used for CPN.
-#         flag_fragmented_inplace: Whether to flag fragmented labels. Flagging sets labels that consist of more than one
-#             connected component to ``constant``.
-#         constant: Flagging constant.
-#         raise_fragmented: Whether to raise ValueError when encountering fragmented labels.
-#
-#     Returns:
-#         dict
-#     """
-#     crops = []
-#     contours = OrderedDict()
-#     for channel in np.split(labels, labels.shape[2], 2):
-#         crops += [(p.label, p.image) + p.bbox[:2] for p in regionprops(channel)]
-#     for label, crop, oy, ox in crops:
-#         crop.dtype = np.uint8
-#         r = cv2.findContours(crop, mode=mode, method=method, offset=(ox, oy))
-#         if len(r) == 3:  # be compatible with both existing versions of findContours
-#             _, c, _ = r
-#         elif len(r) == 2:
-#             c, _ = r
-#         else:
-#             raise NotImplementedError('try different cv2 version')
-#         try:
-#             c, = c
-#         except ValueError as ve:
-#             if flag_fragmented_inplace:
-#                 labels[labels == label] = constant
-#             elif raise_fragmented:
-#                 raise ValueError('Object labeled with multiple connected components.')
-#             continue
-#         if len(c) == 1:
-#             c = np.concatenate((c, c), axis=0)  # min len for other functions to work properly
-#         contours[label] = c
-#     if labels.shape[2] > 1:
-#         return OrderedDict(sorted(contours.items()))
-#     return contours
-#
-#
-# class _BBBC039:
-#     def __init__(self, directory, mode: str):
-#         assert mode in ('train', 'test', 'val')
-#
-#         meta_directory = join(directory, 'metadata')
-#         masks_directory = join(directory, 'masks')
-#         images_directory = join(directory, 'images')
-#
-#         self.names = read_all(meta_directory, {
-#             'train': 'training.txt',
-#             'val': 'validation.txt',
-#             'test': 'test.txt'
-#         }[mode])
-#
-#         self.images, self.masks, self.labels = load(images_directory, masks_directory, self.names)
-#
-#     def plot(self, num=1, figsize=(20, 15)):
-#         for i in np.random.randint(0, len(self), num):
-#             show_detection(image=self.images[i], contours=labels2contours(self.labels[i]),
-#                            figsize=figsize, contour_linestyle='-')
-#
-#     def __getitem__(self, item):
-#         return self.names[item], self.images[item], self.masks[item], self.labels[item]
-#
-#     def __len__(self):
-#         return len(self.images)
-#
-#
-# class BBBC039Train(_BBBC039):
-#     def __init__(self, directory):
-#         super().__init__(directory, mode='train')
-#
-#
-# class BBBC039Val(_BBBC039):
-#     def __init__(self, directory):
-#         super().__init__(directory, mode='val')
-#
-#
-# class BBBC039Test(_BBBC039):
-#     def __init__(self, directory):
-#         super().__init__(directory, mode='test')
 
 class BBBC_039_data(Dataset):
-    def __init__(self, image_dir, mask_dir, transform=None):
+    def __init__(self, image_dir, mask_dir, transform=None, convert_to_rgb=False):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.transform = transform
         self.images = os.listdir(image_dir)
         self.masks = os.listdir(mask_dir)
+        self.convert_to_rgb = convert_to_rgb
 
     def __len__(self):
         return len(self.images)
@@ -129,8 +20,16 @@ class BBBC_039_data(Dataset):
         img_path = os.path.join(self.image_dir, self.images[idx])
         mask_path = os.path.join(self.mask_dir, self.images[idx])
         image = np.array(Image.open(img_path))
-        mask = np.array(Image.open(mask_path).convert('L'), dtype=np.float32)
+
+        if self.convert_to_rgb:
+            image = np.repeat(image[:, :, np.newaxis], 3, axis=2)
+
+        mask = Image.open(mask_path)
+        mask = np.array(mask, dtype=np.float32)
+        mask = mask[..., 2]
         mask[mask == 255.0] = 1.0
+        # get only the contour
+        # mask = mask[..., 2]
 
         if self.transform is not None:
             augmentations = self.transform(image=image, mask=mask)
--- UNET/model.py
+++ UNET/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision.transforms.functional as TF
+import torchvision.models as models
 
 
 class DoubleConv(nn.Module):
@@ -17,6 +18,17 @@ class DoubleConv(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
+
+
+class ConvReLu(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super(ConvReLu, self).__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                              stride=stride, padding=padding)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.relu(self.conv(x))
 
 
 class UNET(nn.Module):
@@ -42,8 +54,7 @@ class UNET(nn.Module):
 
     def forward(self, x):
         skip_connections = []
-
-        for down in self.downs:
+        for idx, down in enumerate(self.downs):
             x = down(x)
             skip_connections.append(x)
             x = self.pool(x)
@@ -67,6 +78,81 @@ class UNET(nn.Module):
             x = self.ups[idx+1](concat_skip)
 
         return self.final_conv(x)
+
+
+class ResUNET(nn.Module):
+    def __init__(self, encoder, out_channels=1, freeze_encoder=True):
+        super(ResUNET, self).__init__()
+        self.encoder = encoder
+
+        if freeze_encoder:
+            for param in self.encoder.parameters():
+                param.requires_grad = not freeze_encoder
+
+        # assuming that the encoder is a resnet18
+        self.encoder_layers = list(encoder.children())
+        self.layer_0 = nn.Sequential(*self.encoder_layers[:3])  # size=(N, 64, x.H/2, x.W/2)
+        self.layer_0_1x1 = ConvReLu(64, 64, kernel_size=1, stride=1, padding=0)
+        self.layer_1 = nn.Sequential(*self.encoder_layers[3:5])  # size=(N, 64, x.H/4, x.W/4)
+        self.layer_1_1x1 = ConvReLu(64, 64, kernel_size=1, stride=1, padding=0)
+        self.layer_2 = self.encoder_layers[5]  # size=(N, 128, x.H/8, x.W/8)
+        self.layer_2_1x1 = ConvReLu(128, 128, kernel_size=1, stride=1, padding=0)
+        self.layer_3 = self.encoder_layers[6]  # size=(N, 256, x.H/16, x.W/16)
+        self.layer_3_1x1 = ConvReLu(256, 256, kernel_size=1, stride=1, padding=0)
+        self.layer_4 = self.encoder_layers[7]  # size=(N, 512, x.H/32, x.W/32)
+        self.layer_4_1x1 = ConvReLu(512, 512, kernel_size=1, stride=1, padding=0)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.conv_up3 = ConvReLu(256 + 512, 512, kernel_size=3, padding=1)
+        self.conv_up2 = ConvReLu(128 + 512, 256, kernel_size=3, padding=1)
+        self.conv_up1 = ConvReLu(64 + 256, 256, kernel_size=3, padding=1)
+        self.conv_up0 = ConvReLu(64 + 256, 128, kernel_size=3, padding=1)
+
+        self.conv_original_size0 = ConvReLu(3, 64, kernel_size=3, padding=1)
+        self.conv_original_size1 = ConvReLu(64, 64, kernel_size=3, padding=1)
+        self.conv_original_size2 = ConvReLu(64 + 128, 64, kernel_size=3, padding=1)
+
+        self.conv_last = nn.Conv2d(64, out_channels, 1)
+
+    def forward(self, x):
+        x_original = self.conv_original_size0(x)
+        x_original = self.conv_original_size1(x_original)
+
+        layer0 = self.layer_0(x)
+        layer1 = self.layer_1(layer0)
+        layer2 = self.layer_2(layer1)
+        layer3 = self.layer_3(layer2)
+        layer4 = self.layer_4(layer3)
+
+        layer4 = self.layer_4_1x1(layer4)
+        x = self.upsample(layer4)
+        layer3 = self.layer_3_1x1(layer3)
+        x = torch.cat([x, layer3], dim=1)
+        x = self.conv_up3(x)
+
+        x = self.upsample(x)
+        layer2 = self.layer_2_1x1(layer2)
+        x = torch.cat([x, layer2], dim=1)
+        x = self.conv_up2(x)
+
+        x = self.upsample(x)
+        layer1 = self.layer_1_1x1(layer1)
+        x = torch.cat([x, layer1], dim=1)
+        x = self.conv_up1(x)
+
+        x = self.upsample(x)
+        layer0 = self.layer_0_1x1(layer0)
+        x = torch.cat([x, layer0], dim=1)
+        x = self.conv_up0(x)
+
+        x = self.upsample(x)
+        x = torch.cat([x, x_original], dim=1)
+        x = self.conv_original_size2(x)
+
+        out = self.conv_last(x)
+
+        return out
 
 
 def test():
